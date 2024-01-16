440f98dcc6df813cbe9ea2ed7c7505e005aded3f
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-01-16 10:49:26 +0900

    version3-firstcommit
    version3 첫번째 커밋

--- Practice.java
+++ Practice.java
@@ -2,5 +2,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("마스터 브랜치");
         System.out.println("version2 브랜치");
+        System.out.println("version3 브랜치");
     }
 }
